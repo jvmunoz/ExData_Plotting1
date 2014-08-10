d28c42426b42c22bdf9fe3aaa9e25d8c26e6f71d
jvmunoz <josev.munoz@gmail.com>
2014-08-10 20:13:55 +0200

    Exploratory Data Analysis

---
+++ plot2.R
@@ -0,0 +1,53 @@
+# establish the working area
+
+mainDir <- "C:/Users/JoseVicente/Desktop/Data Science/Johns Hopkins/Exploratory Data Analysis/Week1/ExData_Plotting1"
+subDir <- "data"
+
+if(getwd()!=mainDir)
+{
+        setwd(mainDir)
+}
+
+if (!file.exists(subDir)){
+        dir.create("./data", showWarnings = FALSE)
+}
+
+
+# download and unzip data
+
+url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(url, "./data/PM25data.zip")
+unzip("./data/PM25data.zip", exdir = "./data")
+
+
+# read data
+
+dataSet <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                    check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+dataSet$Date <- as.Date(dataSet$Date, format="%d/%m/%Y")
+
+
+# subset data to consider
+
+dataSubset <- subset(dataSet, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(dataSet)
+
+
+# transform dates
+
+datetime <- paste(as.Date(dataSubset$Date), dataSubset$Time)
+dataSubset$Datetime <- as.POSIXct(datetime)
+
+
+# Plot required
+
+plot(dataSubset$Global_active_power~dataSubset$Datetime,
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+
+
+# save to png file
+
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,69 @@
+# establish the working area
+
+mainDir <- "C:/Users/JoseVicente/Desktop/Data Science/Johns Hopkins/Exploratory Data Analysis/Week1/ExData_Plotting1"
+subDir <- "data"
+
+if(getwd()!=mainDir)
+{
+        setwd(mainDir)
+}
+
+if (!file.exists(subDir)){
+        dir.create("./data", showWarnings = FALSE)
+}
+
+
+# download and unzip data
+
+url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(url, "./data/PM25data.zip")
+unzip("./data/PM25data.zip", exdir = "./data")
+
+
+# read data
+
+dataSet <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                    check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+dataSet$Date <- as.Date(dataSet$Date, format="%d/%m/%Y")
+
+
+# subset data to consider
+
+dataSubset <- subset(dataSet, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(dataSet)
+
+
+# transform dates
+
+datetime <- paste(as.Date(dataSubset$Date), dataSubset$Time)
+dataSubset$Datetime <- as.POSIXct(datetime)
+
+
+# Plot reqired
+
+with(dataSubset, {
+        
+        plot(Sub_metering_1~Datetime,
+             type="l",
+             xlab="",
+             ylab="Global Active Power (kilowatts)")
+        
+        lines(Sub_metering_2~Datetime,
+              col='Red')
+        
+        lines(Sub_metering_3~Datetime,
+              col='Blue')
+        
+        legend("topright",
+               col=c("black", "red", "blue"),
+               lty=1,
+               lwd=2, 
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        }
+)
+
+
+# save to png file
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
